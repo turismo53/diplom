03e91ec1b71e1c0f26eeefe0445abf7a1b5b3485
turismo53 <turismo53@gmail.com>
2020-04-09 18:26:31 +0300

    fixing links

--- resources/views/auth/orders/show.blade.php
+++ resources/views/auth/orders/show.blade.php
@@ -22,7 +22,7 @@
                         @foreach ($order->products as $product)
                             <tr>
                                 <td>
-                                    <a href="{{ route('product', $product) }}">
+                                    <a href="{{ route('product',[$product->category->code, $product->code] ) }}">
                                         <img height="56px"
                                              src="{{ Storage::url($product->image) }}">
                                         {{ $product->name }}
