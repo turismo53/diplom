15531589e1770964d910b0edb732ed29d9efbd26
turismo53 <turismo53@gmail.com>
2020-07-16 19:24:14 +0300

    another tets fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id')->nullable();
-            $table->string('name')->nullable();;
-            $table->string('email')->unique()->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();;
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at');
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
